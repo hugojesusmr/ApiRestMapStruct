338a1c5c09f198c3339bbfc1133db952a6cb4230
r4ng3l <hugojesusmr@gmail.com>
2022-06-16 01:53:08 +0000

    init repo

---
+++ src/main/java/com/example/ApiRestMapStruct/ApiRestMapStructApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiRestMapStruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestMapStructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestMapStructApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApiRestMapStruct/ApiRestMapStructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiRestMapStruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestMapStructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
